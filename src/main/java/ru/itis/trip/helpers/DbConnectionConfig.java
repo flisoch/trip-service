8a2413b970bc52ee5933ad974c1a9e8ae0006669
flisoch <flisochcs@gmail.com>
2018-10-21 18:00:41 +0300

    db address changed

--- src/main/java/ru/itis/trip/helpers/DbConnectionConfig.java
+++ src/main/java/ru/itis/trip/helpers/DbConnectionConfig.java
@@ -8,12 +8,14 @@ public class DbConnectionConfig {
 
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/abrWork";
+    private static final String URL = "jdbc:postgresql://localhost:5432/carservice";
     private static Connection connection;
 
     public static Connection getConnection() {
+        System.out.println("in connection");
         if(connection == null){
             try {
+                System.out.println("in connection1");
                 Class.forName("org.postgresql.Driver");
                 connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             } catch (SQLException e) {
@@ -22,6 +24,7 @@ public class DbConnectionConfig {
                 e.printStackTrace();
             }
         }
+        System.out.println(connection);
         return connection;
     }
 }
