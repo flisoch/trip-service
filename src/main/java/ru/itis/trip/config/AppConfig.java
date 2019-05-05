709e3d30efe246439e652c2ab79a8449c7c5d23d
flisoch <flisochcs@gmail.com>
2019-05-05 22:33:23 +0300

    configs to use spring data jpa

--- src/main/java/ru/itis/trip/config/AppConfig.java
+++ src/main/java/ru/itis/trip/config/AppConfig.java
@@ -3,20 +3,34 @@ package ru.itis.trip.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaDialect;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
+@EnableJpaRepositories(basePackages = "ru.itis.trip.dao")
+@EnableTransactionManagement
 @ComponentScan(value = "ru.itis.trip")
 @PropertySource("classpath:application.properties")
 @Import(WebAppInitializer.class)
@@ -45,19 +59,45 @@ public class AppConfig {
         return dataSource;
     }
 
-  /*  @Bean
-    public UserService userService() {
-        return new UserServiceImpl(userDao());
-    }
-
-    @Bean
-    public UserDao userDao() {
-        return new UserDaoImpl(dataSource());
-    }*/
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.POSTGRESQL);
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("ru.itis.trip.entities");
+        factory.setDataSource(dataSource);
+        factory.setJpaDialect(jpaDialect());
+        factory.setJpaProperties(hibernateProperties());
+        return factory;
+    }
+
+    @Bean
+    public JpaDialect jpaDialect() {
+        return new HibernateJpaDialect();
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        String[] propsNames = {"hibernate.hbm2ddl.auto", "hibernate.dialect",
+                "hibernate.show_sql", "hibernate.enable_lazy_load_no_trans"};
+        Arrays.stream(propsNames).forEach(propName ->
+                hibernateProperties.setProperty(propName,
+                        environment.getProperty(propName)));
+        return hibernateProperties;
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
     }
 
 
